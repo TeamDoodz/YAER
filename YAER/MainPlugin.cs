d38c3a0791b781ba52e1bc93812e27fb98ad61f5
TeamDoodz <unconfigured@null.spigotmc.org>
2022-02-07 11:32:07 -0800

    fix version number part 2

--- YAER/MainPlugin.cs
+++ YAER/MainPlugin.cs
@@ -15,7 +15,7 @@ namespace YAER {
 
 		internal const string GUID = "io.github.TeamDoodz." + Name;
 		internal const string Name = "YAER";
-		internal const string Version = "1.0.0";
+		internal const string Version = "0.1.1";
 
 		internal static ManualLogSource logger;
 
